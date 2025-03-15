f6770c9b526326d30bf9f832e1e5cd2aa2a844cc
LuizGxDev <dev.luizgabryel@proton.me>
2025-03-14 21:02:40 -0300

    ffff

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: './', // Garante que os caminhos sejam relativos e funcionem na Vercel
+  base: '', // Garante que os caminhos sejam relativos e funcionem na Vercel
   build: {
     outDir: 'dist', // Gera a pasta 'dist' na raiz do projeto
     emptyOutDir: true, // Limpa a pasta 'dist' antes de gerar novos arquivos
